b5f60ff19e18d03a43db5991a57a0dc6d3767004
Felix Dumit <felix.dumit@gmail.com>
2013-08-27 23:46:41 -0300

    test storing tweets in DB

--- twitter_test.py
+++ twitter_test.py
@@ -1,13 +1,20 @@
 from twython import Twython
+from pymongo import Connection
+
+
+db = Connection(
+    'mongodb://test:test@paulo.mongohq.com:10063/TweetCat').TweetCat
 
 APP_KEY = 'CVP4Tgh3FZahPl6wVSf6NQ'
 APP_SECRET = 'aYbYRnYjbtDzJjTWsqjp10vKbxzSrB46CnuQtwZMI'
 OAUTH_TOKEN = '76013996-9XJdUtMi78dQ3OiFIBuXqziNDVwBK6B1noh4eUu5w'
 OAUTH_TOKEN_SECRET = 'BoihAhEBpydRZJGB5RsUPTPrKTxTzW3Fejr88MFl0'
 
+USERNAME = 'ESPN'
+
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-r= twitter.get_user_timeline(screen_name='ESPN')
+r = twitter.get_user_timeline(screen_name=USERNAME)
 
-print '\n---------\n'.join([x.get('text').encode('utf-8') for x in r]) 
-
+for x in r:
+    db.tweets.save({'_id': x['id_str'], 'class': USERNAME, 'text': x['text']})
