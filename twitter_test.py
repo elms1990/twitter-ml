1eb81c52c1992c2505c2c15d9b81960ac80ff6b0
Felix Dumit <felix.dumit@gmail.com>
2013-08-28 14:51:58 -0300

    added get tweets function, organized into functions

--- twitter_test.py
+++ twitter_test.py
@@ -2,19 +2,28 @@ from twython import Twython
 from pymongo import Connection
 
 
-db = Connection(
-    'mongodb://test:test@paulo.mongohq.com:10063/TweetCat').TweetCat
+def get_tweets(db, **kwargs):
+    return [x for x in db.tweets.find(kwargs)]
 
-APP_KEY = 'CVP4Tgh3FZahPl6wVSf6NQ'
-APP_SECRET = 'aYbYRnYjbtDzJjTWsqjp10vKbxzSrB46CnuQtwZMI'
-OAUTH_TOKEN = '76013996-9XJdUtMi78dQ3OiFIBuXqziNDVwBK6B1noh4eUu5w'
-OAUTH_TOKEN_SECRET = 'BoihAhEBpydRZJGB5RsUPTPrKTxTzW3Fejr88MFl0'
 
-USERNAME = 'ESPN'
+def download_tweets(db, user, num=20):
+    db.tweets.remove()
 
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    APP_KEY = 'CVP4Tgh3FZahPl6wVSf6NQ'
+    APP_SECRET = 'aYbYRnYjbtDzJjTWsqjp10vKbxzSrB46CnuQtwZMI'
+    OAUTH_TOKEN = '76013996-9XJdUtMi78dQ3OiFIBuXqziNDVwBK6B1noh4eUu5w'
+    OAUTH_TOKEN_SECRET = 'BoihAhEBpydRZJGB5RsUPTPrKTxTzW3Fejr88MFl0'
 
-r = twitter.get_user_timeline(screen_name=USERNAME)
+    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-for x in r:
-    db.tweets.save({'_id': x['id_str'], 'class': USERNAME, 'text': x['text']})
+    r = twitter.get_user_timeline(screen_name=user)
+
+    for x in r:
+        db.tweets.save({'_id': x['id_str'], 'classn': user, 'text': x['text']})
+
+
+if __name__ == '__main__':
+	db = Connection(
+	    'mongodb://test:test@paulo.mongohq.com:10063/TweetCat').TweetCat
+    download_tweets(db, 'ESPN')
+    print get_tweets(db, classn='ESPN')
