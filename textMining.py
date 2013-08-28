e847e56da10808d55f380daabd36ce55b8958ec8
Padilha <rafapadilha>
2013-08-28 16:10:03 -0300

    Comecando a funcao buildDictionary

--- textMining.py
+++ textMining.py
@@ -1,10 +1,11 @@
 """ Script de text mining """
+from twitter_test import get_tweets_text
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.snowball import EnglishStemmer
 import string
 
-tweet = "Arsenal's got their swagger back. The Gunners just qualified qualify for the Champions League Group Stage for the 16th straight year. @nba_nbb #victory"
+#tweet = "RT Arsenal's got their swagger back. The Gunners just qualified qualify for the Champions League Group Stage for the 16th straight year. @nba_nbb #victory"
 
 # tokenizeTweet(tweet) receives a string representing the tweet and parses it into tokens
 # stemming them as possible. Discard @users but saves #hashtags 
@@ -16,13 +17,16 @@ def tokenizeTweet(tweet):
     allWords = [word.lower() for sentence in sent_tokenize(tweet)
                 for word in word_tokenize(sentence)]
 
-    #deletes @users and saves #hashtags
+    #deletes @users and RT and saves #hashtags
     nWords, i = len(allWords), 0
     hashtags = []
     while i < nWords:
         if allWords[i] == '@':
             allWords[i:i+2] = []
             nWords-=2
+        elif allWords[i] == 'rt':
+            allWords[i:i+1] = []
+            nWords-=1
         elif allWords[i] == '#':
             hashtags.append(allWords[i+1])
             allWords[i:i+2] = []
@@ -41,5 +45,11 @@ def tokenizeTweet(tweet):
         tokens.append('#'+tag)
     return tokens
 
+## Work In Progress
+def buildDictionary():
+    tweetList = [each.encode('ascii','ignore') for each in get_tweets_text()]
+    print tweetList
+
+
 if __name__ == '__main__':
-    print tokenizeTweet(tweet)
+    buildDictionary()
