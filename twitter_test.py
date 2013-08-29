8f05cf92a335a342a02147f68217f00bfff14484
Felix Dumit <felix.dumit@gmail.com>
2013-08-29 13:46:01 -0300

    use dics to save twitter accs and cats

--- twitter_test.py
+++ twitter_test.py
@@ -1,7 +1,5 @@
 from twython import Twython
 from pymongo import Connection
-from threading import Thread
-
 
 db = Connection(
     'mongodb://test:test@paulo.mongohq.com:10063/TweetCat').TweetCat
@@ -15,7 +13,7 @@ def get_tweets_text(**kwargs):
     return [x['text'] for x in db.tweets.find(kwargs)]
 
 
-def download_tweets(users, cat, num=20):
+def download_tweets(accs, num=20):
     #db.tweets.remove()
 
     APP_KEY = 'CVP4Tgh3FZahPl6wVSf6NQ'
@@ -24,27 +22,35 @@ def download_tweets(users, cat, num=20):
     OAUTH_TOKEN_SECRET = 'BoihAhEBpydRZJGB5RsUPTPrKTxTzW3Fejr88MFl0'
 
     twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-    for user in users:
+    for acc in accs:
         i = 0
         total = 0
         while True:
-            r = twitter.get_user_timeline(screen_name=user, count=200, page=i)
+            r = twitter.get_user_timeline(screen_name=acc['user'], count=200, page=i)
             for x in r:
                 db.tweets.save(
-                    {'_id': x['id_str'], 'classn': cat, 'text': x['text'].encode('ascii', 'ignore'), 'user': user})
+                    {'_id': x['id_str'], 'classn': acc['cat'], 'text': x['text'].encode('ascii', 'ignore'), 'user': acc['user']})
             if len(r) == 0:
                 break
             else:
                 i += 1
                 total += len(r)
 
-            print 'saved %s tweets' % total
-        print 'saved all tweets from: %s' % user
+            print 'saved %s tweets from %s' % (total, acc['user'])
+        print 'saved all tweets from: %s' % acc['user']
 
 
 if __name__ == '__main__':
-    #download_tweets(['ESPN', 'SkySports', 'BBCSport','FOXSports'], 'sports')
-    download_tweets(['Gizmodo', 'CNET', 'twitter','google'], 'tech')
+    download_tweets([
+        {'user':'ESPN', 'cat':'sports'},
+        {'user':'SkySports', 'cat': 'sports'},
+        {'user':'BBCSport', 'cat':'sports'},
+        {'user':'FOXSports', 'cat':'sports'},
+        {'user':'Gizmodo', 'cat':'tech'}, 
+        {'user':'CNET', 'cat':'tech'},
+        {'user': 'google', 'cat':'tech'},
+        {'user': 'BBBCTech', 'cat':'tech'}
+        ])
 
 
     print len(get_tweets_text())
