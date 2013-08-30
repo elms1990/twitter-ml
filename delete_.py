e26930d4c926a7939b446ccde0b1227aad3b4de1
Felix Dumit <felix.dumit@gmail.com>
2013-08-29 21:26:23 -0300

    add gitignore

--- delete_.py
+++ delete_.py
@@ -1,4 +1,17 @@
 import nltk
 
-if __name__ == '__main__':
-	nltk.download()
+from pymongo import Connection
+
+db = Connection(
+    'mongodb://test:test@paulo.mongohq.com:10063/TweetCat').TweetCat
+
+for x in db.tweets.find():
+	x['new'] = False
+	db.tweets.save(x)
+
+b =  [x for x in db.tweets.find({'user':'google'}).sort('_id',-1).limit(4)]
+print b
+
+
+# if __name__ == '__main__':
+# 	nltk.download()
