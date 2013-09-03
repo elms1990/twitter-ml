6d9b5a013940c6c8570eeaa88ca493a9e33d568e
Padilha <rafapadilha>
2013-09-03 17:42:06 -0300

    Alguns comentarios bestas que logo serao deletados, mas sao necessarios para programar durante aulas idiotas e sem internet (CB - bittencourt)

--- tweetSVM.py
+++ tweetSVM.py
@@ -23,3 +23,20 @@ def createExampleFile():
 
 if __name__ == '__main__':
 	pass
+
+
+
+
+
+	"""How to use
+<line> .=. <target> <feature>:<value> <feature>:<value> ... <feature>:<value> # <info>
+<target> .=. +1 | -1 | 0 | <float> 
+<feature> .=. <integer> | "qid"
+<value> .=. <float>
+<info> .=. <string>
+The target value and each of the feature/value pairs are separated by a space character. Feature/value pairs MUST be ordered by increasing feature number. Features with value zero can be skipped. The string <info> can be used to pass additional information to the kernel (e.g. non feature vector data). Check the FAQ for more details on how to implement your own kernel.
+
+In classification mode, the target value denotes the class of the example. +1 as the target value marks a positive example, -1 a negative example respectively. So, for example, the line
+
+-1 1:0.43 3:0.12 9284:0.2 # abcdef
+"""
