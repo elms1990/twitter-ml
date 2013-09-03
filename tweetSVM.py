cad00fa748f9d5435171cdf71e6f0a7eb6ae9d5b
Padilha <rafapadilha>
2013-09-03 17:31:59 -0300

    Comecando funcao tweetSVM.createExampleFile

--- tweetSVM.py
+++ tweetSVM.py
@@ -9,9 +9,17 @@ It uses somethings from the script svmlight.py from Clint Burfoot
 import svmlight.py
 
 categoryList = ['sports','tech']
-dictionaryFilePath = "whole_dictionary.txt"
+dictionaryFile = "whole_dictionary.txt"
 featuresFilePath = "_features.txt"
 #############################################################################
+
+# createExampleFile() gets every tweet/vector of features from the features files
+# and writes them to the file that is going to be used by svm_learn
+# IN: dictionary = FreqDist, categoryList = [str]
+# OUT: nothing
+def createExampleFile():
+	pass
+
 
 if __name__ == '__main__':
 	pass
