3e7f19474bc2f7a38450bd55a7755f8dc10af851
Padilha <rafapadilha>
2013-09-04 15:10:49 -0300

    Adding decisionTree.py

---
+++ decisionTree.py
@@ -0,0 +1,13 @@
+# decisionTree.py ################################
+"""
+Script description
+"""
+#
+
+# Defines and imports ######
+from sklearn import tree
+
+#
+
+if __name__ == '__main__':
+	pass
